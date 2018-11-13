d8ead380830936334c18ae02216b9d4e2e3c8bc1
jungbinn <wjdqls7958@kaist.ac.kr>
2018-11-13 20:33:53 +0900

    apply django form

--- webdemo/views.py
+++ webdemo/views.py
@@ -1,5 +1,21 @@
+import os
+import sys
+sys.path.append(os.path.dirname(os.path.abspath(os.path.dirname(__file__))))
+
 from django.shortcuts import render
 from .models import Chat
+from .forms import ChatForm
+import ChatSummarize
 
 def chat_summarize(request):
-    return render(request, 'webdemo/chat_summarize.html', {})
+    if request.method == "POST":
+        form = ChatForm(request.POST)
+        if form.is_valid():
+            text = form.save(commit=False)
+            chatSummarizer = ChatSummarize.ChatSummarizer(text.chat)
+            chatSummarizer.preprocess()
+            summary = chatSummarizer.summarize(4)
+            return render(request, 'webdemo/chat_summarize.html', {'form': form, 'summary': summary})
+    else:
+        form = ChatForm()
+    return render(request, 'webdemo/chat_summarize.html', {'form': form})
