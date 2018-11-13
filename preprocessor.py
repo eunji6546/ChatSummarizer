54e7ebf3edbc1a0c419cba1d62bbfe02d48ff855
eunji6546 <eunji6546@gmail.com>
2018-11-13 15:47:39 +0900

    preprocessor as class

--- preprocessor.py
+++ preprocessor.py
@@ -5,35 +5,31 @@ from hanspell import spell_checker as sc
 
 from lexrankr import LexRank
 
-if __name__=='__main__':
-	
-	cw = CW_conv.Coinedword(file="./coined_word/coinedword_dic.txt") 
+class Preprocessor:
 
 	datapath = "./sample_data/"
-	for fname in os.listdir(datapath):
-		if fname.startswith("chat9"):
-			print(fname)
-			ts = TS_conv.Toksen(datapath + fname)
-			connected_lines = ts.by_time_connect()
-			print(connected_lines)
-			print("connected as sentences ")
-			cw_lines = [cw.convert(x) for x in connected_lines]
-			print("replaced")
-			# Noise Dector should be added 
-			# Spelling Checker 
-			print(cw_lines)
-			sc_lines = [sc.check(x) for x in cw_lines]
-			print("spell_checked")
-			# KoNLPy
-			# LexRank Algorithm 
-			sc_lines = [x.checked for x in sc_lines]
 
-			print(sc_lines)
+	def __init__(self, fname):
+		self.fname = fname 
+		self.cw = CW_conv.Coinedword(file="./coined_word/coinedword_dic.txt") 
+		self.ts = TS_conv.Toksen(self.datapath + self.fname)
+
+	def run(self):
+
+		print ("running file:%s", self.fname)
+		print("connected as sentences ")
+		cw_lines = [self.cw.convert(x) for x in connected_lines]
+		print("spell checking ")
+		sc_lines = [sc.check(x) for x in cw_lines]
+		
+		sc_lines = [x.checked for x in sc_lines]
+
+		print("doing lexrank ")
 			
-			lexrank = LexRank()
-			lexrank.summarize(" ".join(sc_lines))
-			summaries = lexrank.probe(4)  # `num_summaries` can be `None` (using auto-detected topics)
-			for summary in summaries:
-				print(summary)
-			break
+		lexrank = LexRank()
+		lexrank.summarize(" ".join(sc_lines))
+		summaries = lexrank.probe(4)  # `num_summaries` can be `None` (using auto-detected topics)
+		for summary in summaries:
+			print(summary)
+		return summaries
 
