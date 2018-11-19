a2b13495b33e6aa388b1b253444f8681232ec2e0
jungbinn <wjdqls7958@kaist.ac.kr>
2018-11-19 23:03:10 +0900

    create html

--- webdemo/web.py
+++ webdemo/web.py
@@ -1,22 +1,27 @@
 from http.server import BaseHTTPRequestHandler, HTTPServer
  
 # HTTPRequestHandler class
-class requestHandler(BaseHTTPRequestHandler):
+class httpRequestHandler(BaseHTTPRequestHandler):
  
-  # GET
-  def do_GET(self):
-        # Send response status code
+    def set_headers(self):
         self.send_response(200)
- 
-        # Send headers
-        self.send_header('Content-type','text/html')
+        self.send_header('Content-type', 'text/html')
         self.end_headers()
+
+    # GET
+    def do_GET(self):
+        self.set_headers()
  
         # Send message back to client
         message = "Hello world!"
         # Write content as utf-8 data
         self.wfile.write(bytes(message, "utf8"))
         return
+
+    # POST
+    def do_POST(self):
+        self.set_headers()
+
  
 def run():
   print('starting server...')
@@ -24,7 +29,7 @@ def run():
   # Server settings
   # Choose port 8080, for port 80, which is normally used for a http server, you need root access
   server_address = ('127.0.0.1', 8080)
-  httpd = HTTPServer(server_address, requestHandler)
+  httpd = HTTPServer(server_address, httpRequestHandler)
   print('running server...')
   httpd.serve_forever()
  
