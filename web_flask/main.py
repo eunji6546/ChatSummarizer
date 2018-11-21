e4e2d1cac63700c63616fa7402f9b25085d5f5ea
jungbinn <wjdqls7958@kaist.ac.kr>
2018-11-21 16:16:43 +0900

    basic flask - summarize not working yet

---
+++ web_flask/main.py
@@ -0,0 +1,28 @@
+import os
+import sys
+sys.path.append(os.path.dirname(os.path.abspath(os.path.dirname(__file__))))
+
+import ChatSummarize
+
+from flask import Flask, render_template, request
+app = Flask(__name__)
+
+@app.route('/')
+def main():
+    return render_template('chat_summarizer.html')
+
+@app.route('/summarize', methods=['GET', 'POST'])
+def summarize():
+    if request.method == 'POST':  # POST request
+        post_params = request.form
+        chat = post_params['chatField']
+        chatSummarizer = ChatSummarize.ChatSummarizer(chat)
+        chatSummarizer.preprocess()
+        summary = chatSummarizer.summarize(4)
+        return render_template('chat_summarizer.html', summary=summary)
+
+    else: # GET request
+        return render_template('chat_summarizer.html')
+
+if __name__ == '__main__':
+    app.run()
